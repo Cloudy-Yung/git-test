55dc17f167d0b19e8e1b8f8e37ec29d777dddcd1
Cloud.Y <yang.zhao11@qq.com>
2019-07-12 21:35:53 +0800

    把本地工程添加到git

---
+++ git-first-project/git-first/src/com/javaee44/git/FirstTest.java
@@ -0,0 +1,11 @@
+package com.javaee44.git;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("hello git first project");
+        System.out.println("hello git first project");
+        System.out.println("hello git first project");
+        System.out.println("hello git first project");
+        System.out.println("hello git first project");
+    }
+}
